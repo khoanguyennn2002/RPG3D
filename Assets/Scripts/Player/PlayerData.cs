3404eba77f8283d6e0230dcd8d4eb4a541bb1d91
khoanguyennn2002 <113275717+khoanguyennn2002@users.noreply.github.com>
2024-02-23 22:21:00 +0700

    Create Attribute

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,8 +1,24 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class PlayerData : ScriptableObject
 {
-    
+    public List<Attribute> attributes;
+}
+
+[System.Serializable]
+public class Attribute
+{
+    public string Name;
+    public int Value;
+    public string Description;
+
+    //public Attribute(string name, int value, string description)
+    //{
+    //    Name = name;
+    //    Value = value;
+    //    Description = description;
+    //}
 }
